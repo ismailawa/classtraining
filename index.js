22fc451dfa9c93bc285699ee9929a85904a8fe43
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2021-05-18 10:42:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var a; // Declaration global variable
+var c;
+var d;
+a = 5; // Assignment
+d = 10;
+
+console.log(a); // Printing to console
+
+var b = "this is javascript"; // Declaration and Assignment
+
+console.log(b);
+
+c = 100.67;
+console.log(c);
+
+function add(){
+    var e = 20; // local variable
+    var f = 30;
+    console.log(a+d);
+}
+
+function sub(){
+    // console.log(e - f);
+}
+
+add();
+
+sub();
+
