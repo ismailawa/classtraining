f5b19fa63a2188a6ec83e8af6033006dd6480caf
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2021-06-08 10:20:27 +0100

    assignment done

--- object.js
+++ object.js
@@ -49,20 +49,61 @@ var students = [
         id: 2,
         name: "femi",
         age: 25
+    },
+    {
+        id: 6,
+        name: "sunday",
+        age: 29
+    },
+    {
+        id: 3,
+        name: "shuaibu",
+        age: 19
+    },
+    {
+        id: 4,
+        name: "kunle",
+        age: 28
+    },
+    {
+        id: 5,
+        name: "david",
+        age: 28
     }
 ]
 
-console.log(students.filter((s)=> s.id == 1))
+let maxAge = 0
 
-for(let s of students){
-    console.log(s.name)
-}
+students.forEach((student)=>{
+    if(student.age > maxAge){
+        maxAge = student.age
+    }
+})
 
-for(let i=0; i < students.length; i++){
-    console.log(students[i].name)
-}
+let oldestStudent = students.filter((student)=> student.age == maxAge)
+console.log(oldestStudent)
 
-console.log(Object.keys(students))
+let studentName = students.map((student)=>{
+    const { name } = student;
+    return { name }
+})
+
+const avgAge = students.map((student)=> student.age).reduce((prev,next)=> prev + next)/students.length
+
+
+console.log(avgAge)
+
+// console.log(students.filter((s)=> s.id == 1))
+
+// for(let s of students){
+//     console.log(s.name)
+// }
+
+// for(let i=0; i < students.length; i++){
+//     console.log(students[i].name)
+// }
+
+// console.log(Object.keys(students))
 
 
 
